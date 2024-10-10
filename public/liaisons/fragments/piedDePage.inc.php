a83b9f7203728241310ad6db8ec0597b002232bf
Laurie Roy <laurie.roy4@gmail.com>
2024-10-09 20:30:26 -0400

    Rajout du html du pied de page

--- public/liaisons/fragments/piedDePage.inc.php
+++ public/liaisons/fragments/piedDePage.inc.php
@@ -1,5 +1,69 @@
 <footer class="piedDePage">
-    <h5 class="piedDePage__copyRights">@Copyrights Michel Rouleau</h5>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">LE OFF</a>
+                <ul>
+                    <li><a href="#">Tarifs</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Programmation</a>
+                <ul>
+                    <li><a href="#">Par lieu</a></li>
+                    <li><a href="#">Par dates</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Artistes</a>
+                <ul>
+                    <li><a href="#">Par style musical</a></li>
+                    <li><a href="#">Artistes A-Z</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Partenaires</a>
+                <ul>
+                    <li><a href="#">Devenir partenaires</a></li>
+                    <li><a href="#">Partenaires actuels</a></li>
+                </ul>
+            </li>
+    </nav>
+    <div class="informations__secondaires">
+        <div class="nos__coordonnees">
+            <h2>Nos coordonnées</h2>
+                <address>
+                    <p>Festival OFF de Québec<br>
+                       82 boul. René-Lévesque Ouest<br>
+                       C.P. 48010<br>
+                       Québec, Québec<br>
+                       G1S 4X2</p>
+                       <p><a href="mailto:info@quebecoff.org">info@quebecoff.org</a><br>
+                       <a href="mailto:media@quebecoff.org">media@quebecoff.org</a>
+                    </p>
+                </address>
+        </div>
+        <div class="medias">
+            <a href="https://www.facebook.com/?locale=fr_CA">
+                <img class="sociaux" src="../public/liaisons/images/svg/ic--baseline-facebook.svg" alt="Facebook">
+            </a>
+            <a href="https://www.youtube.com/">
+                <img class="sociaux" src="../public/liaisons/images/svg/mdi--youtube.svg" alt="Youtube">
+            </a>
+            <a href="https://twitter.com/?lang=fr">
+                <img class="sociaux" src="../public/liaisons/images/svg/prime--twitter.svg" alt="Twitter">
+            </a>
+            <a href="https://www.instagram.com/">
+                <img class="sociaux" src="../public/liaisons/images/svg/mdi--instagram.svg" alt="Instagram">
+            </a>
+        </div>
+        <br>
+        <div id="logo_footer">
+            <a href="#">
+            <img src="../public/liaisons/images/svg/logo_OFF.svg" alt="Festival-OFF" title="Festival-OFF" /></a>
+        </div>
+             <h5 class="piedDePage__copyRights">© 2009-2024 Festival OFF Tous droits réservés</h5>
 </footer>
 
 <script src="liaisons/js/_menu.js"></script>
