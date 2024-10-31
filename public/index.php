a68aa8e388c5295a7d6dfd5fe45458b7e2dd1a83
Marie-Pierre Plante <1943813@csfoy.ca>
2024-10-31 12:38:00 -0400

    un commit

--- public/index.php
+++ public/index.php
@@ -6,11 +6,12 @@
 <html lang="fr">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<meta name="keyword" content="">
-	<meta name="author" content="">
+	<meta name="description" content="Site web OFF, Festival OFF de Quebec">
+	<meta name="keyword" content="HTML, CSS, JavaScript, OFF, musique , festival musique">
+	<meta name="author" content=" Marie-Pierre Plante">
 	<meta charset="utf-8">
-	<title>Un beau titre ici!</title>
+	<link rel="icon" type="image/x-icon" href="">
+	<title> O FF - Festival OFF De Québec </title>
 	<?php include ($niveau . "liaisons/fragments/headlinks.inc.html");?>
 </head>
 
@@ -32,13 +33,41 @@
 			<?php } ?>
 
 			<section>
-				<h3>Entête de section</h3>
-				<article>
-					<header>
-						<h4>Entête d'article</h4>
-					</header>
-					<p>Lorem ipsum dolor HTML5 nunc aut nunquam sit amet, consectetur adipiscing elit. Vivamus at est eros, vel fringilla urna.</p>
-					<p>Per inceptos himenaeos. Quisque feugiat, justo at vehicula pellentesque, turpis lorem dictum nunc.</p>
+			<div id="contenu" class="conteneur">
+			<section class="article">
+				<h2>Articles</h2>
+				<?php foreach($arrArticles as $article){ ?>
+					<article>
+						<header>
+							<h3><?php echo $article['titre'];?></h3>
+						</header>
+						<p>
+							<?php echo $article['article_preview'];?>
+							<a href="#">...</a>
+						</p>
+						<footer>
+							<p> <?php echo $article['auteurs']; ?></p>
+							<time datetime="<?php echo $article['date_complete'];?>">
+								<?php echo "Le " . $article['jour'] . " " . $arrMois[$article['mois'] - 1] . " " . $article['annee'] . " à " . $article['heure'] . "h" . $article['minute'];?>
+							</time>
+						</footer>
+					</article>
+				<?php } ?>
+			</section>
+			<section class="suggestions">
+				<h2>Artistes suggérés</h2>
+				<ul>
+					<?php foreach($arrArtistesSuggestion as $artiste){ ?>
+						<li>
+							<a href="<?php echo $niveau ?>artistes/fiches/index.php?id_artiste=<?php echo $artiste['id']; ?>">
+								<?php echo $artiste['nom']; ?>
+								<!-- Sources des images pas encore définies par l'équipe. Vérification sur le alt -->
+								<img src="#" alt="<?php echo "ID artiste : " . $artiste['id'] ?>">
+							</a>
+						</li>
+					<?php } ?>
+				</ul>
+		</div>
 					<footer>
 						<h5>Pied d'article</h5>
 					</footer>
